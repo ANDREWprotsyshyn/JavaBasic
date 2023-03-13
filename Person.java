b734deaa0a5073bce9672afcde4c321eb3288e70
ANDREWprotsyshyn <126571666+ANDREWprotsyshyn@users.noreply.github.com>
2023-03-13 12:45:28 +0100

    Add files via upload

---
+++ Person.java
@@ -0,0 +1,23 @@
+public class Person {
+    public static int Andrii;
+
+    public static void main(String[] args) {
+     String name = "Andrii";
+     String lastName = "Protsyshyn";
+     int year0fBirth = 2002;
+     int age = 20;
+     float height = 186F;
+     String gender = "Male";
+     boolean sunAndBoy = true;
+     String hobbies = "IT,Football,Cars";
+        System.out.println(lastName);
+        System.out.println(year0fBirth);
+        System.out.println(age);
+        System.out.println(height);
+        System.out.println(gender);
+        System.out.println(sunAndBoy);
+        System.out.println(hobbies);
+
+    }
+
+}
